ceaca50f3e4b8dcf84716ee6b895a1b3edb76a1d
Jaimin Pandya <Jaimin.p@alufplastics.com>
2018-09-06 15:04:06 -0400

    All tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,36 @@
 use std::collections::HashMap;
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-  unimplemented!(
-    "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-    nucleotide,
-    dna
-  );
+  let nucleotides = vec!['A', 'T', 'C', 'G'];
+  if !nucleotides.contains(&nucleotide) {
+    return Err(nucleotide);
+  }
+  let dna_vec = Vec::from(dna);
+  dna_vec
+    .iter()
+    .map(|&nucleotide_u8| {
+      if nucleotide_u8 as char == nucleotide {
+        return Ok(1);
+      } else if !nucleotides.contains(&(nucleotide_u8 as char)) {
+        return Err(nucleotide_u8 as char);
+      }
+      return Ok(0);
+    })
+    .sum::<Result<usize, char>>()
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-  unimplemented!(
-    "How much of every nucleotide type is contained inside DNA string '{}'?",
-    dna
-  );
+  let nucleotides = vec!['A', 'T', 'C', 'G'];
+  let mut result: HashMap<char, usize> = HashMap::new();
+  for nucleotide in nucleotides {
+    match count(nucleotide, dna) {
+      Ok(count) => {
+        result.insert(nucleotide, count);
+      }
+      Err(nucleotide) => {
+        return Err(nucleotide);
+      }
+    }
+  }
+  Ok(result)
 }
